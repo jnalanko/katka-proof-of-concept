f1e7b7e51163c64bea0c028926cdee859b3c0b88
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-03-12 22:50:19 +0200

    To graph

--- LCA_part.cpp
+++ LCA_part.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <unordered_map>
 #include <bit>
+#include <set>
 #include "LCA_support.hh"
 #include "Tree.hh"
 
@@ -37,6 +38,41 @@ vector<MEM> load_MEM_data(const string& filename){
     return mems;
 }
 
+vector<int64_t> get_all_ancestors(shared_ptr<Tree> T, int64_t v){
+    vector<int64_t> ancestors;
+    while(v != 0){
+        ancestors.push_back(v);
+        v = T->nodes[v].parent_id;
+    } ancestors.push_back(v); // Root
+    return ancestors;
+}
+
+
+void print_subgraph_in_dot_format(shared_ptr<Tree> T, const vector<int64_t>& LCA_nodes){
+    set<int64_t> subgraph_nodes, marked_nodes;
+    for(int64_t v : LCA_nodes){
+        marked_nodes.insert(v);
+        for(int64_t u : get_all_ancestors(T, v)){
+            subgraph_nodes.insert(u);
+        }
+    }
+
+    // Print the DOT graph
+
+    cout << "digraph graphname {" << endl;
+    for(int64_t v : subgraph_nodes){
+
+        // Add node
+        string color = marked_nodes.count(v) ? "red" : "black";
+        cout << "  " << v << " [style=filled, fillcolor=" << color << "];" << endl;
+
+        // Add edge from parent
+        cout << "  " << T->nodes[v].parent_id << " -> " << v << ";" << endl;
+
+    }
+    cout << "}" << endl;
+}
+
 int main(int argc, char** argv){
 
     string tree_encoding = read_file(argv[1]);
@@ -48,11 +84,14 @@ int main(int argc, char** argv){
     shared_ptr<Tree> T = make_unique<Tree_Loader>()->load_tree(tree_encoding);
     LCA_support LCA(T);
 
+    // Find the LCAs
+    vector<int64_t> LCA_nodes;
     for(MEM mem : MEM_data){
-        if(mem.length > 20){
+        if(mem.length > 0){
             int64_t lca = LCA.LCA(mem.left_doc, mem.right_doc);
-            cout << lca << endl;
+            LCA_nodes.push_back(lca);
         }
     }
 
+    print_subgraph_in_dot_format(T, LCA_nodes);
 }
