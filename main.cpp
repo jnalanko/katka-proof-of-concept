9e16e147b30f02018a513f742b07953aac1c755b
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-03-12 15:19:08 +0200

    Store children

--- main.cpp
+++ main.cpp
@@ -24,13 +24,16 @@ class Tree{
 public:
     struct Node{
         string name;
+        int64_t id;
         int64_t parent_id;
         vector<int64_t> children_ids;
+        vector<double> children_edge_lengths;
     };
 
     vector<Node> nodes; // node[0] is the root
-
 };
+
+Tree T;
 
 // Returns next comma at the current level of the tree.
 // If not found, returns end + 1
@@ -62,9 +65,13 @@ int64_t find_colon(const string& tree_encoding, int64_t left, int64_t right){
 }
 
 // [left, right] is a segment of tree_encoding
-void traverse_subtree(const string& tree_encoding, int64_t left, int64_t right){
+// Returns the node id of the root of the subtree
+int64_t traverse_subtree(const string& tree_encoding, int64_t left, int64_t right, int64_t parent_id){
 
-    cout << "Subtree " << tree_encoding.substr(left, right-left+1) << endl;
+    // Create a new node (added to the tree at the end of the function
+    Tree::Node v;
+    v.parent_id = parent_id;
+    v.id = T.nodes.size(); // Create a new id
 
     // Subtree -> Leaf | Internal
 
@@ -76,7 +83,7 @@ void traverse_subtree(const string& tree_encoding, int64_t left, int64_t right){
         int64_t open = left;
         int64_t close = find_close(tree_encoding, left);
         string name = tree_encoding.substr(close+1, right - (close+1) + 1);
-        cout << "Internal node name: " << name << endl;
+        v.name = name;
 
         // BranchSet → Branch | Branch "," BranchSet
         int64_t branchset_start = open+1;
@@ -103,9 +110,10 @@ void traverse_subtree(const string& tree_encoding, int64_t left, int64_t right){
                 if(subtree_end < branch_end){
                     // Length is present
                     string length = tree_encoding.substr(subtree_end+1+1, branch_end - subtree_end - 1);
-                    cout << "Length " << length << endl;
+                    v.children_edge_lengths.push_back(stod(length));
                 }
-                traverse_subtree(tree_encoding, subtree_start, subtree_end);
+                int64_t child_id = traverse_subtree(tree_encoding, subtree_start, subtree_end, v.id);
+                v.children_ids.push_back(child_id);
             } else{
                 // Subtree is a leaf
                 int64_t colon_pos = find_colon(tree_encoding, branch_start, branch_end);
@@ -113,9 +121,11 @@ void traverse_subtree(const string& tree_encoding, int64_t left, int64_t right){
                 int64_t subtree_end = branch_end;
                 if(colon_pos != -1){
                     subtree_end = colon_pos - 1;
-                    cout << "Length: " << tree_encoding.substr(colon_pos + 1, branch_end - (colon_pos+1) + 1) << endl;
+                    string length = tree_encoding.substr(colon_pos + 1, branch_end - (colon_pos+1) + 1);
+                    v.children_edge_lengths.push_back(stod(length));
                 }
-                traverse_subtree(tree_encoding, subtree_start, subtree_end);
+                int64_t child_id = traverse_subtree(tree_encoding, subtree_start, subtree_end, v.id);
+                v.children_ids.push_back(child_id);
             }
 
             branch_start = branch_end + 1 + 1; // Skip over comma or closing ')'
@@ -124,8 +134,11 @@ void traverse_subtree(const string& tree_encoding, int64_t left, int64_t right){
         }
     }  else{
         // Leaf → Name
-        cout << "Leaf name: " << tree_encoding.substr(left, right-left+1) << endl;
+        v.name = tree_encoding.substr(left, right-left+1);
     }
+
+    T.nodes.push_back(v);
+    return v.id;
 }
 
 
@@ -135,5 +148,9 @@ int main(int argc, char** argv){
     // Trim trailing whitespace
     while(tree_encoding.back() == ' ' || tree_encoding.back() == '\n') tree_encoding.pop_back();
 
-    traverse_subtree(tree_encoding, 0, tree_encoding.size()-1-1); // Discard the ';' in the end.
+    traverse_subtree(tree_encoding, 0, tree_encoding.size()-1-1, 0); // Discard the ';' in the end.
+
+    for(Tree::Node v : T.nodes){
+        cout << v.id << " " << v.parent_id << endl;
+    }
 }
