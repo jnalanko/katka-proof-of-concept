11c48b6237daa06fa7859193163bc8a798ddd33b
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-03-13 12:34:43 +0200

    MEM tests pass

--- run_MEM_test.bash
+++ run_MEM_test.bash
@@ -3,11 +3,16 @@ mkdir -p temp
 make MEMs
 make brute_MEMs
 
-./brute_MEMs test_data/random_ref.fna test_data/random_query.fna > temp/correct.txt
-./MEMs test_data/random_ref.fna test_data/random_query.fna > temp/ours.txt
+./brute_MEMs test_data/random_ref.fna test_data/random_query.fna --lex-ranges > temp/lex_correct.txt
+./MEMs test_data/random_ref.fna test_data/random_query.fna --lex-ranges > temp/lex_ours.txt
 
-head temp/*
+./brute_MEMs test_data/random_ref.fna test_data/random_query.fna --doc-ranges > temp/doc_correct.txt
+./MEMs test_data/random_ref.fna test_data/random_query.fna --doc-ranges > temp/doc_ours.txt
 
-sha1sum temp/correct.txt temp/ours.txt
+head temp/lex_correct.txt temp/lex_ours.txt
+head temp/doc_correct.txt temp/doc_ours.txt
+
+sha1sum temp/lex_correct.txt temp/lex_ours.txt
+sha1sum temp/doc_correct.txt temp/doc_ours.txt
 
 
